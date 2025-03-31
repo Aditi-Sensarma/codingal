0dcb5c02c40feb377d9b44145b0d1a34172ba893
Aditi <aditisensarma@gmail.com>
2025-03-31 16:44:12 +0530

    learnt about print

--- python/class 1/intro.py
+++ python/class 1/intro.py
@@ -1,1 +1,12 @@
 print("hello")
+
+
+# github  <----- remote space where u have stored ur project
+
+
+# git < ----- it is a software that I installed on local system
+
+
+# 1. git add .
+# 2. git commit -m "learnt about print"
+# 3. git push
