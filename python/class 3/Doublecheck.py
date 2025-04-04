c9a941afdfa6b96e9638b75de2648a28214ed42b
Aditi <aditisensarma@gmail.com>
2025-04-04 15:42:47 +0530

    learnt about print

---
+++ python/class 3/Doublecheck.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number : "))
+
+if num > 50 :
+    print("Greater than 50")
+    if num%2 == 0 :
+        print("It is even")
+    else : 
+        print("It is odd")
+else : 
+    print("Less than 50")
