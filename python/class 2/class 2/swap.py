59d7bc278e49df5a2e3ba99b956aeed581821321
Aditi <aditisensarma@gmail.com>
2025-04-06 10:57:43 +0530

    learnt about print

---
+++ python/class 2/class 2/swap.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a value : "))
+y = int(input("Enter another value : "))
+
+temp = x
+x = y
+y = temp
+
+print(f"Value of x after swapping {x} and value of y after swapping {y}")
