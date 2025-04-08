18036891e135e951525bb463e5fe606697fb664a
Aditi <aditisensarma@gmail.com>
2025-04-08 15:31:16 +0530

    learnt about print

---
+++ python/class 4/for.py
@@ -0,0 +1,14 @@
+for i in range (0,20,3):
+    print("Aditi i",i)
+
+for i in range (10,150,3):
+    print(i)
+
+for i in range (100,600):
+    print(i,"* 13 = ", i*13)
+
+for i in range(1,1000):
+    if i%2 == 0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
